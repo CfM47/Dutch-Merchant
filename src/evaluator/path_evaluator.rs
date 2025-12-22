d66397570ac851b09073765e81f2ed69b2417389
Jossué Arteche Muñoz <jossuearteche000@gmail.com>
2025-12-22 10:34:33 -0500

    fix: modified decisions in output to be a tuple

--- src/evaluator/path_evaluator.rs
+++ src/evaluator/path_evaluator.rs
@@ -11,9 +11,8 @@ pub trait PathEvaluator {
     /// # Returns
     ///
     /// A tuple containing the best profit and the decisions of buying and selling at each node.
-    /// These are to be interpreted as d[i][m] = amount of good m bought (if positive) or sold (if
-    /// negative) at port with index i in the nodes array, note that port Ids may repeat in this array.
-    fn calculate_best_profit(&self, instance: &Instance, nodes: &[PortId]) -> (f64, Vec<Vec<f64>>);
+    /// These are to be interpreted as d[i][m] = (amount of good m bought, amount of good sold) at port i.
+    fn calculate_best_profit(&self, instance: &Instance, nodes: &[PortId]) -> (f64, Vec<Vec<(f64, f64)>>);
 
     fn name(&self) -> &'static str;
 }
